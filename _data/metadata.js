d236722d8da2410b81b68e1039b3a88372c3244b
Koushik Sridhar <koushik.sridhar@swapcard.com>
2024-05-02 10:20:45 +0200

    Adding details and removing the message

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Koushik's Sec Blog",
+	url: "https://seceveryday.github.io/myblog/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my Security Expertise ",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Koushik Krishnamurthy Sridhar",
+		email: "koushikks.edu@gmail.com",
+		url: "https://seceveryday.github.io/"
 	}
 }
